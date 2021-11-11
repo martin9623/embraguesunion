ebbe601dd96a36c81bd895e47be05706a7320a56
martin9623 <martingomez.9623@gmail.com>
2021-11-11 15:42:07 -0300

    first release

---
+++ script/script.js
@@ -0,0 +1,20 @@
+const BOTON = document.querySelector(".header__nav__btn");
+const ICON = document.querySelector(".header__nav__btn__icon");
+const MENU = document.querySelector(".header__nav__menu");
+const LINK = document.querySelector(".header__nav__menu__list__item__link");
+
+const MENU_ACTIVO = (x) => {
+  if (x) {
+    x.addEventListener("click", () => {
+      ICON.classList.toggle("fa-times");
+      MENU.classList.toggle("menu-activo");
+    });
+  }
+};
+
+MENU_ACTIVO(BOTON);
+MENU_ACTIVO(LINK[0]);
+MENU_ACTIVO(LINK[1]);
+MENU_ACTIVO(LINK[2]);
+MENU_ACTIVO(LINK[3]);
+MENU_ACTIVO(LINK[4]);
