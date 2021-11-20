1fe2e961aac492862e916f97633888bf4b3e226d
martin9623 <martingomez.9623@gmail.com>
2021-11-20 17:46:58 -0300

    web v2

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ const BOTON = document.querySelector(".header__nav__btn");
 const ICON = document.querySelector(".header__nav__btn__icon");
 const MENU = document.querySelector(".header__nav__menu");
 const LINK = document.querySelector(".header__nav__menu__list__item__link");
+const FORM = document.getElementById("form");
 
 const MENU_ACTIVO = (x) => {
   if (x) {
@@ -18,3 +19,9 @@ MENU_ACTIVO(LINK[1]);
 MENU_ACTIVO(LINK[2]);
 MENU_ACTIVO(LINK[3]);
 MENU_ACTIVO(LINK[4]);
+
+if (FORM) {
+  FORM.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+}
